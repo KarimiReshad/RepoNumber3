e22484259d7db9568823c75e4ed80e6fed10f873
KarimiReshad <117615941+KarimiReshad@users.noreply.github.com>
2022-11-10 23:35:46 -0500

    Update ThisIsPractice.java
    main method and extrabranch removed.

--- src/ThisIsPractice.java
+++ src/ThisIsPractice.java
@@ -4,16 +4,7 @@ public class ThisIsPractice {
 	public static void main(String[] args) {
 
 
-		System.out.println("This is a do or die for me");
 		
-		
-		
-		
-	}
-	
-	public void testMethod() {
-		System.out.println("this method is on extrabRANCH");
-	}
 	
 	
 
